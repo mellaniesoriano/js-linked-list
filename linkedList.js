b016f74809143f76840c7693a556b59e190681f5
mellaniesoriano <msoriano808@gmail.com>
2017-05-24 20:54:18 -1000

    getHead, getTail passes, working on add() function

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,47 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+  var head = null;
+  var tail = null;
 
+  function getHead() {
+    return head;
+  }
+
+  function getTail() {
+    return tail;
+  }
+
+  function add(value) {
+    // console.log(value);
+    var newNode = {
+      value : value,
+      next : null
+    };
+    if (getHead() === null) {
+      return newNode;
+    }
+
+  }
+
+  function get() {
+
+  }
+
+  function remove() {
+
+  }
+
+  function insert() {
+
+  }
+
+return {
+  getHead,
+  getTail,
+  add,
+  get,
+  remove,
+  insert
+}
 }
