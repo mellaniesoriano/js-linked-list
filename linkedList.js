bdfd671a493116edff93ea51542713fec7225c12
mellaniesoriano <msoriano808@gmail.com>
2017-05-25 21:11:24 -1000

    FINALLY DONE -___-

--- linkedList.js
+++ linkedList.js
@@ -15,6 +15,7 @@ function linkedListGenerator(){
     return tail;
   }
 
+
   function add(value) {
     // console.log(value);
     var newNode = {
@@ -37,26 +38,50 @@ function linkedListGenerator(){
 
   function get(num) {
     var checkNode = head;
-    var count = 0;
 
     for (var i = 0; i < num; i++) {
-      if (checkNode.next !== null) {
-        checkNode = checkNode.next;
-        count++;
-      } else {
+      if (checkNode.next === null) {
         return false;
+      } else {
+        checkNode = checkNode.next;
       }
     }
     return checkNode;
   }
 
-  function remove() {
+  function remove(num) {
+    var removeNode = get(num);
+    var removeNodeParent = get(num - 1);
 
+    if (removeNode === false) {
+      return false;
+    } else if (num === 0) {
+      head = head.next;
+    } else if (removeNode.next === null) {
+      tail = removeNodeParent;
+    }
+      removeNodeParent.next = removeNode.next;
   }
 
-  function insert() {
+  function insert(value, num) {
+    console.log('value >> ', value, 'num >> ', num);
+    var currentNode = get(num - 1);
 
-  }
+    if (num === 0) {
+      newNode1 = {
+      value : value,
+      next : head
+    };
+    head = newNode1;
+    } else if (get(num) === false || num < 1) {
+      return false;
+    } else {
+      currentNode.next = {
+        value : value,
+        next : currentNode.next
+      };
+    }currentNode = newNode2;
+}
 
 return {
   getHead,
